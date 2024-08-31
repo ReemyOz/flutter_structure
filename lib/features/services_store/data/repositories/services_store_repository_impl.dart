735b8a6c6103a5855037267a39050b738a16f1cd
Reem Onez <reem66onez@gmail.com>
2024-08-31 20:37:08 +0300

    add services store repository implementation class

---
+++ lib/features/services_store/data/repositories/services_store_repository_impl.dart
@@ -0,0 +1,10 @@
+import 'package:clean_architecture/core/resources/data_state.dart';
+import 'package:clean_architecture/features/services_store/data/models/services_store_model.dart';
+import 'package:clean_architecture/features/services_store/domain/repositories/services_store_repository.dart';
+
+class ServicesStoreRepositoryImpl implements ServicesStoreRepository {
+  @override
+  Future<DataState<ServicesStoreModel>> getStoreServices() {
+    throw UnimplementedError();
+  }
+}
--- lib/features/services_store/domain/repositories/services_store_repository.dart
+++ lib/features/services_store/domain/repositories/services_store_repository.dart
@@ -1,6 +1,6 @@
 import 'package:clean_architecture/core/resources/data_state.dart';
 import 'package:clean_architecture/features/services_store/domain/entities/services_store_entity.dart';
 
-abstract class ServicesStoreRepositories {
+abstract class ServicesStoreRepository {
   Future<DataState<ServicesStoreEntity>> getStoreServices();
 }
